eebdf58ada3d194eccd7557925a94f8de7c695f2
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2020-11-05 19:31:02 +0300

    changed file

--- sc.sql
+++ sc.sql
@@ -1,41 +1,53 @@
-create table country
-(
-    id           int auto_increment
-        primary key,
-    country_name varchar(30) not null
-);
+# Сделать запрос, в котором мы выберем все данные о городе – регион, страна.
+select c.id, c.title, r.title, ct.title
+from _cities as c
+         left join _regions as r on c.region_id = r.id
+         left join _countries as ct on r.country_id = ct.id
+order by c.id
 
-create table area_place
-(
-    id              int auto_increment
-        primary key,
-    name_area_place varchar(30) not null,
-    country_id      int         not null,
-    constraint area_place_country_id_fk
-        foreign key (country_id) references country (id)
-);
+# Выбрать все города из Московской области.
+select c.title
+from _cities as c
+         left join _regions as r on c.region_id = r.id
+where r.title like 'Московская область'
 
-create table district
-(
-    id            int auto_increment
-        primary key,
-    name_district varchar(30) not null,
-    area_place_id int         not null,
-    constraint district_area_place_id_fk
-        foreign key (area_place_id) references area_place (id)
-);
 
-create table city
-(
-    id            int auto_increment
-        primary key,
-    name_city     varchar(30) not null,
-    area_place_id int         not null,
-    district_id   int         null,
-    constraint city_area_place_id_fk
-        foreign key (area_place_id) references area_place (id),
-    constraint city_district_id_fk
-        foreign key (district_id) references district (id)
-);
+# Выбрать среднюю зарплату по отделам.
+select d.dept_name, avg(s.salary) as average_salary
+from salaries as s
+         inner join employees e on s.emp_no = e.emp_no
+         inner join dept_emp de on e.emp_no = de.emp_no
+         inner join departments d on d.dept_no = de.dept_no
+group by d.dept_no, d.dept_name
 
+# Выбрать максимальную зарплату у сотрудника.
+select e.emp_no, e.first_name, e.last_name, max(s.salary) as max_salary
+from salaries as s
+         join employees e on e.emp_no = s.emp_no
+group by e.emp_no
+order by max_salary desc
+
+# Удалить одного сотрудника, у которого максимальная зарплата.
+delete
+from employees e
+where e.emp_no = (select x.emp_no
+                  from (
+                           select e.emp_no, max(s.salary) as max_salary
+                           from salaries as s
+                                    join employees e on e.emp_no = s.emp_no
+                           group by e.emp_no
+                           order by max_salary desc
+                           limit 1) as x)
+
+# Посчитать количество сотрудников во всех отделах
+select de.dept_no, dept_name, count(de.emp_no) from dept_emp de
+left join departments d on d.dept_no = de.dept_no
+group by de.dept_no
+
+
+# найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел
+select de.dept_no, dept_name, count(distinct de.emp_no), sum(salary) from dept_emp de
+left join departments d on d.dept_no = de.dept_no
+left join salaries s on de.emp_no = s.emp_no
+group by de.dept_no
 
